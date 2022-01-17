cd836385f6d2d128424623dbb07d9e34815bd8a7
Han-Archives <77054461+Han-Archives@users.noreply.github.com>
2022-01-17 09:49:40 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: `Han-기록소`,
-  description: `Han-archives`,
+  description: `Han- 분석 기록`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://https://han-archives.github.io/`,
+  siteUrl: `https://han-archives.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -10,6 +10,19 @@ module.exports = {
     },
   },
   ga: '0', // Google Analytics Tracking ID
+  author: {
+    name: `정진혁`,
+    bio: {
+      role: `개발자`,
+      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+    },
+    social: {
+      github: ``, // `https://github.com/zoomKoding`,
+      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: ``, // `zoomkoding@gmail.com`,
+    },
+  },
 
   // metadata for About Page
   about: {
@@ -29,6 +42,15 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
+      {
+        date: '2021.02 ~',
+        activity: '개인 블로그 개발 및 운영',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
     ],
 
     projects: [
